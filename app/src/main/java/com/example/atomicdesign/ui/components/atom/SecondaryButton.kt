cae271aece532b668adba89734c039eb9f6bfc69
Angel <4mr0m3r0@gmail.com>
2021-03-07 17:42:03 -0300

    Applying OutlinedButton to SecondaryButton.kt

--- app/src/main/java/com/example/atomicdesign/ui/components/atom/SecondaryButton.kt
+++ app/src/main/java/com/example/atomicdesign/ui/components/atom/SecondaryButton.kt
@@ -1,14 +1,12 @@
 package com.example.atomicdesign.ui.components.atom
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults.buttonColors
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.atomicdesign.ui.theme.AtomicDesignSampleTheme
@@ -16,15 +14,14 @@ import java.util.*
 
 @Composable
 fun SecondaryButton(label: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
-    Button(
+    OutlinedButton(
         onClick = onClick,
         border = BorderStroke(1.dp, MaterialTheme.colors.primary),
-        colors = buttonColors(backgroundColor = Color.White),
         modifier = modifier
     ) {
         Text(
             text = label.toUpperCase(Locale.getDefault()),
-            color = MaterialTheme.colors.primary
+            color = MaterialTheme.colors.secondary
         )
     }
 }
